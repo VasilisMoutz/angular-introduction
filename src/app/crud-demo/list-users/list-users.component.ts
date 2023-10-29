d532a5de2224e63b08b814adac26fb50e7571722
Moutz <basilismoutz@gmail.com>
2023-10-29 11:16:37 +0200

    Get List CRUD

--- src/app/crud-demo/list-users/list-users.component.ts
+++ src/app/crud-demo/list-users/list-users.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppService } from 'src/app/app.service';
+import { Person } from 'src/app/interfaces/person';
 
 @Component({
   selector: 'app-list-users',
@@ -8,6 +10,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './list-users.component.html',
   styleUrls: ['./list-users.component.css']
 })
-export class ListUsersComponent {
+export class ListUsersComponent implements OnInit{
 
+  //Initialise Array
+  users: Person[] = []
+  //Inject service
+  constructor(private service: AppService){}
+
+  ngOnInit(): void {
+    this.service.getAllUsers().subscribe(users => {
+      this.users = users
+    })
+  }
 }
