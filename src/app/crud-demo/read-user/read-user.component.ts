f3351e8d5b335a968d11289139605ee94d945fe2
Moutz <basilismoutz@gmail.com>
2023-10-30 21:23:00 +0200

    read user

--- src/app/crud-demo/read-user/read-user.component.ts
+++ src/app/crud-demo/read-user/read-user.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppService } from 'src/app/app.service';
+import { Person } from 'src/app/interfaces/person';
+import { PersonCardComponent } from 'src/app/person-card/person-card.component';
 
 @Component({
   selector: 'app-read-user',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PersonCardComponent],
   templateUrl: './read-user.component.html',
   styleUrls: ['./read-user.component.css']
 })
 export class ReadUserComponent {
 
+  constructor(private appService: AppService) {}
+
+  @ViewChild('userId') id!: ElementRef<HTMLInputElement>
+
+  user: Person | undefined
+  userNotFound = false
+
+  onClick() {
+    const id = parseInt(this.id.nativeElement.value);
+  
+    this.appService.getUserById(id).subscribe({
+      next: (user) => {
+        this.user = user;
+        this.userNotFound = false
+      },
+      error: (error) => {
+        this.user = undefined;
+        this.userNotFound = true
+        console.log(error)
+      },
+      complete: () => {
+        console.log('Operation Completed')
+      }
+    })
+
+  }
 }
