c9f7ee3513e9688f20da0880f6299f76e75e9292
Moutz <basilismoutz@gmail.com>
2023-11-03 22:00:33 +0200

    Welcome Component and Routing

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { WelcomeComponent } from "./welcome/welcome.component";
+
+export const routes: Routes = [
+    {path: "home", component: WelcomeComponent}
+]
